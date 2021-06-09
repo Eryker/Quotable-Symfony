07270a7121ef756ed51f661987919ac4a946efd2
Oumar <oumar.youssouf0605@gmail.com>
2021-06-09 13:41:25 +0200

    Adding link to Api Quotes

--- src/service/CallApiService.php
+++ src/service/CallApiService.php
@@ -2,11 +2,24 @@
 
 namespace App\Service;
 
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
 
 class CallApiService {
 
+  private $client;
+
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
     public function getQuoteData(): array 
     {
-      return ['test 1', 'test 2'];  
+      $response = $this->client->request(
+        'GET',
+        'https://api.quotable.io/random'
+      );
+      return $response->toArray();
     }
 }
